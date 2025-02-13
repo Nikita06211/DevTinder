b7d74a4530baf0fe9ae12555f0c1c5eee21b47e7
Nikita <bansalnikita06@gmail.com>
2025-02-13 19:36:40 +0530

    exploring get, post

--- src/app.js
+++ src/app.js
@@ -2,11 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use("/about",(req,res)=>{
+// this will only handle GET api calls
+app.get("/user",(req,res)=>{
+    res.send({firstName :"sneha", lastName:"mishra"});
+});
+
+app.post("/user",(req,res)=>{
+    console.log("save data");
+    res.send("data saved");
+});
+
+
+
+// this will match all the http method api calls to "/"
+app.use("/",(req,res)=>{
     res.send("Hello from the server");
 })
 
 app.listen(3001, ()=>{
     console.log("successfully listening on port 3001");
-    
 });
